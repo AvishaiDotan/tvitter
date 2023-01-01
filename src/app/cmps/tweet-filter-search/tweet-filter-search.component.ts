cf3108905d7bef6a86fd41bd2c63bbc6d11269a5
Avishai Dotan <avishaiDotan@gmail.com>
2023-01-01 20:38:17 +0200

    filter

--- src/app/cmps/tweet-filter-search/tweet-filter-search.component.ts
+++ src/app/cmps/tweet-filter-search/tweet-filter-search.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TweetFilter } from 'src/app/models/tweet.model';
+import { TwitterService } from 'src/app/service/twitter.service';
 
 @Component({
-  selector: 'tweet-filter-search',
-  templateUrl: './tweet-filter-search.component.html',
-  styleUrls: ['./tweet-filter-search.component.scss']
+    selector: 'tweet-filter-search',
+    templateUrl: './tweet-filter-search.component.html',
+    styleUrls: ['./tweet-filter-search.component.scss']
 })
 export class TweetFilterSearchComponent {
 
+    constructor(private twitterService: TwitterService) { }
+
+    @Input() tweetFilter!: TweetFilter[]
+    filterToEdit = {term: ''}
+
+    onSetFilter() {
+        this.twitterService.setFilter({ ...this.filterToEdit })
+    }
 }
--- src/app/cmps/tweet-filter/tweet-filter.component.ts
+++ src/app/cmps/tweet-filter/tweet-filter.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TweetFilter } from 'src/app/models/tweet.model';
+import { TwitterService } from 'src/app/service/twitter.service';
 
 @Component({
-  selector: 'tweet-filter',
-  templateUrl: './tweet-filter.component.html',
-  styleUrls: ['./tweet-filter.component.scss']
+    selector: 'tweet-filter',
+    templateUrl: './tweet-filter.component.html',
+    styleUrls: ['./tweet-filter.component.scss']
 })
-export class TweetFilterComponent {
+export class TweetFilterComponent implements OnInit, OnDestroy {
 
+    constructor(private twitterService: TwitterService) { }
+
+    subscription!: Subscription
+    tweetFilter!: TweetFilter
+
+    ngOnInit(): void {
+        this.subscription = this.twitterService.tweetFilter$.subscribe(tweetFilter => {
+            this.tweetFilter = tweetFilter
+        })
+    }
+
+    onSetFilter() {
+        this.twitterService.setFilter({ ...this.tweetFilter })
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe()
+    }
 }
