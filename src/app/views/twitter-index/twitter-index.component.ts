c6a68651ac77f9a9f5f91e6b0b6776d3c7dd2c73
Miriam Smith <miriamzeff@gmail.com>
2023-01-02 22:31:51 +0200

    scroll to top of page when header clicked

--- src/app/views/twitter-index/twitter-index.component.ts
+++ src/app/views/twitter-index/twitter-index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Tweet } from 'src/app/models/tweet.model';
 import { TwitterService } from 'src/app/service/twitter.service';
@@ -9,17 +10,21 @@ import { TwitterService } from 'src/app/service/twitter.service';
     styleUrls: ['./twitter-index.component.scss'],
 })
 export class TwitterIndexComponent implements OnInit {
-    constructor(private twitterService: TwitterService) {}
+    constructor(
+        private router: Router,
+        private twitterService: TwitterService
+    ) {}
 
     tweets!: Tweet[];
     tweets$!: Observable<Tweet[]>;
     selectedTweetId: string = '';
-    
 
     ngOnInit(): void {
         this.twitterService.query();
         this.tweets$ = this.twitterService.tweets$;
     }
 
-
+    scroll(el: HTMLElement) {
+        el.scrollIntoView({behavior: 'smooth'});
+    }
 }
