91f405aea47107f596674515888fc453d958e643
Miriam Smith <miriamzeff@gmail.com>
2023-01-02 23:47:08 +0200

    new userDb and updated tweetDB

--- src/app/service/tweetsDB.ts
+++ src/app/service/tweetsDB.ts
@@ -1,8 +1,33 @@
 export const tweetsDB = [
     {
+        _id: 'a1b2c3d4',
+        tweetImgUrl: 'https://picsum.photos/id/{{tweet._id}}/400/200',
+        text: 'Just finished an awesome game of football with my friends! #football',
+        user: {
+            _id: 'e5f6g7h8',
+            username: 'janeDoe',
+            avatarUrl: 'https://pbs.twimg.com/media/DtHxuHlUwAE7R_b.jpg',
+        },
+        createdAt: 1605343051000,
+        replies: ['i9j0k1l2', 'm3n4o5p6'],
+        likes: [
+            {
+                _id: 'q7r8s9t0',
+                username: 'johnDoe',
+                avatarUrl: 'https://pbs.twimg.com/media/DtHxuHlUwAE7R_b.jpg',
+            },
+            {
+                _id: 'u1v2w3x4',
+                username: 'sarahSmith',
+                avatarUrl: 'https://pbs.twimg.com/media/DtHxuHlUwAE7R_b.jpg',
+            },
+        ],
+    },
+
+    {
         _id: '1',
         tweetImgUrl: 'https://pbs.twimg.com/media/123456789876543210.jpg',
-        text: 'Hello, world!',
+        text: 'Just tried the best sushi in town! #foodie #sushi',
         user: {
             _id: '1',
             username: 'johnsmith',
@@ -22,7 +47,7 @@ export const tweetsDB = [
     },
     {
         _id: '2',
-        text: 'This is a tweet',
+        text: 'So excited to be going on a road trip through the west coast! #travels #roadtrip',
         user: {
             _id: '1',
             username: 'johnsmith',
@@ -43,7 +68,7 @@ export const tweetsDB = [
     {
         _id: '3',
         tweetImgUrl: 'https://pbs.twimg.com/media/098765432123456789.jpg',
-        text: 'This is another tweet',
+        text: "Obsessed with this new yoga class I'm taking. #wellness #yoga",
         user: {
             _id: '2',
             username: 'janesmith',
@@ -65,7 +90,7 @@ export const tweetsDB = [
         _id: '4',
         username: 'john_doe',
         avatarUrl: 'https://example.com/avatars/john_doe.png',
-        text: "I'm loving TypeScript so far!",
+        text: "Who's ready for the big game tonight?! #football #sport",
         user: {
             _id: '4',
             username: 'john_doe',
@@ -90,7 +115,7 @@ export const tweetsDB = [
         _id: '5',
         username: 'jane_doe',
         avatarUrl: 'https://example.com/avatars/jane_doe.png',
-        text: 'TypeScript is a great addition to any project!',
+        text: 'Had the best time exploring the city on my bike today. #cycling #adventure',
         user: {
             _id: '5',
             username: 'jane_doe',
@@ -110,7 +135,7 @@ export const tweetsDB = [
         _id: '6',
         username: 'bob_smith',
         avatarUrl: 'https://example.com/avatars/bob_smith.png',
-        text: "I'm glad to hear that!",
+        text: "Thrilled to announce that I'll be running my first marathon next month! #fitness #running",
         user: {
             _id: '6',
             username: 'bob_smith',
@@ -130,7 +155,7 @@ export const tweetsDB = [
         _id: '8',
         username: 'bob_smith',
         avatarUrl: 'https://example.com/avatars/bob_smith.png',
-        text: "I'm a big fan of TypeScript!",
+        text: "Just finished my latest painting and I'm loving how it turned out. #art #creativity",
         user: {
             _id: '8',
             username: 'bob_smith',
@@ -156,7 +181,8 @@ export const tweetsDB = [
         username: 'jane_doe',
         avatarUrl: 'https://example.com/avatars/jane_doe.png',
         tweetImgUrl: 'https://example.com/tweet_images/9.png',
-        text: 'I agree, TypeScript has really helped me catch bugs before they happen!',
+        text: "So proud of my little brother for scoring the winning goal in his soccer game. #family #sports"
+        ,
         user: {
             _id: '9',
             username: 'jane_doe',
@@ -176,7 +202,8 @@ export const tweetsDB = [
         _id: '10',
         username: 'jane_doe',
         avatarUrl: 'https://example.com/avatars/jane_doe.png',
-        text: 'TypeScript is definitely worth learning!',
+        text: "Can't wait to try out this new vegan restaurant everyone's been raving about. #vegan #foodie"
+        ,
         user: {
             _id: '10',
             username: 'jane_doe',
@@ -197,7 +224,8 @@ export const tweetsDB = [
         username: 'john_doe',
         avatarUrl: 'https://example.com/avatars/john_doe.png',
         tweetImgUrl: 'https://example.com/tweet_images/11.png',
-        text: 'TypeScript has really improved my workflow!',
+        text: "Excited to be visiting a new country for the first time next month! #travels #explore"
+        ,
         user: {
             _id: '11',
             username: 'john_doe',
@@ -222,7 +250,8 @@ export const tweetsDB = [
         _id: '12',
         username: 'jane_doe',
         avatarUrl: 'https://example.com/avatars/jane_doe.png',
-        text: 'I completely agree! TypeScript has saved me so much time and effort.',
+        text: "Feeling so inspired after attending a workshop on mindfulness. #wellness #mindfulness"
+        ,
         user: {
             _id: '12',
             username: 'jane_doe',
@@ -242,7 +271,8 @@ export const tweetsDB = [
         _id: '13',
         username: 'jane_doe',
         avatarUrl: 'https://example.com/avatars/jane_doe.png',
-        text: 'TypeScript is a game-changer!',
+        text: "Just finished building my first app with #reactjs and I'm loving it! #coding #tech"
+        ,
         user: {
             _id: '13',
             username: 'jane_doe',
@@ -285,7 +315,8 @@ export const tweetsDB = [
     {
         _id: '15',
         tweetImgUrl: 'https://example.com/tweet_images/15.png',
-        text: 'TypeScript has made my code so much more reliable!',
+        text: "Can't believe I just hiked to the top of that mountain. Such a rush! #adventure #outdoors"
+        ,
         user: {
             _id: '15',
             username: 'jane_doe',
@@ -302,3 +333,128 @@ export const tweetsDB = [
         ],
     },
 ];
+
+// export const tweetsDB = [
+//     {
+//         _id: 'tweet1',
+//         tweetImgUrl: 'https://api.lorem.space/image?w=150&h=180&hash=8B7BCDC2',
+//         text: 'Just cooked the best pasta dish for dinner! #cooking',
+//         user: {
+//             _id: 'user1',
+//             username: 'jane',
+//             avatarUrl:
+//                 'https://api.lorem.space/image/face?w=150&h=150&hash=8B7BCDC2',
+//         },
+//         createdAt: 1605343051000,
+//         replies: ['tweet2', 'tweet3'],
+//         likes: [
+//             {
+//                 _id: 'user2',
+//                 username: 'john',
+//                 avatarUrl:
+//                     'https://api.lorem.space/image/face?w=150&h=150&hash=500B67FB',
+//             },
+//             {
+//                 _id: 'user3',
+//                 username: 'sarah',
+//                 avatarUrl:
+//                     'https://api.lorem.space/image/face?w=150&h=150&hash=A89D0DE6',
+//             },
+//         ],
+//     },
+//     {
+//         _id: 'tweet2',
+//         tweetImgUrl: 'https://api.lorem.space/image?w=150&h=180&hash=500B67FB',
+//         text: 'Heading off on a trip to Bali! #travels #trips',
+//         user: {
+//             _id: 'user4',
+//             username: 'mike',
+//             avatarUrl:
+//                 'https://api.lorem.space/image/face?w=150&h=150&hash=225E6693',
+//         },
+//         createdAt: 1605343051000,
+//         replies: ['tweet1'],
+//         likes: [
+//             {
+//                 _id: 'user5',
+//                 username: 'lisa',
+//                 avatarUrl:
+//                     'https://api.lorem.space/image/face?w=150&h=150&hash=9D9539E7',
+//             },
+//             {
+//                 _id: 'user6',
+//                 username: 'alex',
+//                 avatarUrl:
+//                     'https://api.lorem.space/image/face?w=150&h=150&hash=BDC01094',
+//             },
+//         ],
+//     },
+//     {
+//         _id: 'tweet3',
+//         tweetImgUrl: 'https://api.lorem.space/image?w=150&h=180&hash=A89D0DE6',
+//         text: 'Just finished building a new project with #reactjs!',
+//         user: {
+//             _id: 'user7',
+//             username: 'samuel',
+//             avatarUrl:
+//                 'https://api.lorem.space/image/face?w=150&h=150&hash=7F5AE56A',
+//         },
+//         createdAt: 1605343051000,
+//         replies: ['tweet1'],
+//         likes: [],
+//     },
+//     {
+//         _id: 'tweet4',
+//         tweetImgUrl: 'https://api.lorem.space/image?w=150&h=180&hash=8B7BCDC2',
+//         text: 'Just caught the winning touchdown at the football game! #football',
+//         user: {
+//             _id: 'user1',
+//             username: 'jane',
+//             avatarUrl:
+//                 'https://api.lorem.space/image/face?w=150&h=150&hash=8B7BCDC2',
+//         },
+//         createdAt: 1605343051000,
+//         replies: ['tweet2', 'tweet3'],
+//         likes: [
+//             {
+//                 _id: 'user2',
+//                 username: 'john',
+//                 avatarUrl:
+//                     'https://api.lorem.space/image/face?w=150&h=150&hash=500B67FB',
+//             },
+//             {
+//                 _id: 'user3',
+//                 username: 'sarah',
+//                 avatarUrl:
+//                     'https://api.lorem.space/image/face?w=150&h=150&hash=A89D0DE6',
+//             },
+//         ],
+//     },
+//     {
+//         _id: 'tweet5',
+//         tweetImgUrl: '',
+//         text: 'Heading off on a trip to Bali! #travels #trips',
+//         user: {
+//             _id: 'user4',
+//             username: 'mike',
+//             avatarUrl:
+//                 'https://api.lorem.space/image/face?w=150&h=150&hash=225E6693',
+//         },
+//         createdAt: 1605343051000,
+//         replies: ['tweet1'],
+//         likes: [
+//             {
+//                 _id: 'user5',
+//                 username: 'lisa',
+//                 avatarUrl:
+//                     'https://api.lorem.space/image/face?w=150&h=150&hash=9D9539E7',
+//             },
+//             {
+//                 _id: 'user6',
+//                 username: 'alex',
+//                 avatarUrl:
+//                     'https://api.lorem.space/image/face?w=150&h=150&hash=BDC01094',
+//             },
+//         ],
+//     },
+// ];
---
+++ src/app/service/userDB.ts
@@ -0,0 +1,62 @@
+export const userDB = [
+    {
+        _id: 'user1',
+        username: 'jane',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=8B7BCDC2',
+    },
+    {
+        _id: 'user2',
+        username: 'john',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=500B67FB',
+    },
+    {
+        _id: 'user3',
+        username: 'sarah',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=A89D0DE6',
+    },
+    {
+        _id: 'user4',
+        username: 'mike',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=225E6693',
+    },
+    {
+        _id: 'user5',
+        username: 'lisa',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=9D9539E7',
+    },
+    {
+        _id: 'user6',
+        username: 'alex',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=BDC01094',
+    },
+    {
+        _id: 'user7',
+        username: 'samuel',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=7F5AE56A',
+    },
+    {
+        _id: 'user8',
+        username: 'david',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=4F32C4CF',
+    },
+    {
+        _id: 'user9',
+        username: 'jessica',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=B0E33EF4',
+    },
+    {
+        _id: 'user10',
+        username: 'matt',
+        avatarUrl:
+            'https://api.lorem.space/image/face?w=150&h=150&hash=2D297A22',
+    },
+];
