e94130a9a8b9bf0f7e42a5a6cb5b7a41e60d8214
Y-Moshe <moshe.nehemiah@gmail.com>
2023-01-03 20:06:50 +0200

    basic profile page

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { lastValueFrom, Subscription } from 'rxjs';
 
 import { UserService } from '../../service/user.service';
-import { User } from 'src/app/models';
+import { TwitterService } from '../../service/twitter.service';
+import { Tweet, User } from 'src/app/models';
 
 @Component({
-  selector: 'profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+    selector: 'profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnDestroy, OnInit {
     user!: User;
-    userSubscription!: Subscription
+    userSubscription!: Subscription;
+    tweets: Tweet[] = [];
 
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private twitterService: TwitterService
+    ) {}
 
     ngOnInit(): void {
-        this.userSubscription = this.userService.user$.subscribe((user) => (this.user = user!));
+        this.userSubscription = this.userService.user$.subscribe(
+            (user) => (this.user = user!)
+        );
+
+        this.tweets = this.twitterService.getAllTweets()
+            .filter(tweet => tweet.user._id === this.user._id)
+    }
+
+    async handleLike(tweet: Tweet) {
+        await lastValueFrom(this.twitterService.toggleLike(tweet._id))
     }
 
     ngOnDestroy(): void {
-        this.userSubscription.unsubscribe()
+        this.userSubscription.unsubscribe();
     }
 }
