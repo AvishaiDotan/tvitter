e848fb6b6f729adbe9ee24393ef5f541e6ebfc9a
Y-Moshe <moshe.nehemiah@gmail.com>
2023-01-03 21:07:49 +0200

    background service for add new tweets

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TwitterService } from '../service/twitter.service';
 import { UserService } from '../service/user.service';
 
 @Component({
@@ -6,12 +7,20 @@ import { UserService } from '../service/user.service';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    constructor(private userService: UserService) {}
-    
+export class AppComponent implements OnInit, OnDestroy {
+    constructor(
+        private userService: UserService,
+        private twitterService: TwitterService
+    ) {}
+
     isOpenedAdvancedSearchModal = false
 
     ngOnInit(): void {
         this.userService.loadUser()
+        this.twitterService.startBackgroundService()
+    }
+
+    ngOnDestroy(): void {
+        this.twitterService.stopBackgroundService()
     }
 }
