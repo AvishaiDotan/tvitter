2d3ed21330e4abebd63d92e0788bdfbc9006f8d9
Avishai Dotan <avishaiDotan@gmail.com>
2023-01-03 14:45:34 +0200

    style reply and like buttons and functionality

--- src/app/cmps/tweet-preview/tweet-preview.component.ts
+++ src/app/cmps/tweet-preview/tweet-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { Tweet } from 'src/app/models/tweet.model';
 
 @Component({
@@ -6,21 +7,26 @@ import { Tweet } from 'src/app/models/tweet.model';
     templateUrl: './tweet-preview.component.html',
     styleUrls: ['./tweet-preview.component.scss']
 })
-export class TweetPreviewComponent {
+export class TweetPreviewComponent{
+
+    constructor(private router: Router) {}
 
     @Input() tweet!: Tweet
 
+    is_animating: boolean = false;
+    isLiked: boolean = false;
+
     getDatePipe(timestamp: number): string {
         if (Date.now() - timestamp < 60000) return 'ss'
-        if (Date.now() - timestamp < 3600000) return'mm'
+        if (Date.now() - timestamp < 3600000) return 'mm'
         return 'LLL d'
     }
 
-    
     @Output() like = new EventEmitter<Tweet>()
 
     likeClick(event: MouseEvent) {
         event.stopPropagation()
         this.like.emit(this.tweet)
+        this.is_animating = !this.is_animating
     }
 }
